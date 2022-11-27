e6863026c756b73a94bc99094a33038f6a30c458
Norbert Szulc <norbert@not7cd.net>
2022-11-28 00:17:03 +0100

    Whoops

--- env.sh
+++ env.sh
@@ -8,20 +8,17 @@ else
 
     AUTHENTIK_URL=auth.$DOMAIN
     MINIO_URL=media.$DOMAIN
-    OUTLINE_URL=outline.$DOMAIN
-
-    PG_PASS=$(pwgen -s 40 1)
-    AUTHENTIK_SECRET_KEY=$(pwgen -s 50 1)
-    MINIO_ACCESS_KEY=$(pwgen -s 16 1)
-    MINIO_SECRET_KEY=$(pwgen -s 64 1)
+    OUTLINE_URL=outline.$DOMAIN 
 
     echo "AUTHENTIK_URL=$AUTHENTIK_URL" >> .env
     echo "MINIO_URL=$MINIO_URL" >> .env
     echo "OUTLINE_URL=$OUTLINE_URL" >> .env
 
-    echo "PG_PASS=$PG_PASS" >> .env
-    echo "PG_PASS_OUTLINE=$PG_PASS" >> .env
-    echo "PG_PASS_AUTHENTIK=$PG_PASS" >> .env
+    echo "PG_PASS=$(pwgen -s 40 1)" >> .env
+    echo "PG_PASS_OUTLINE=$(pwgen -s 40 1)" >> .env
+    echo "PG_PASS_AUTHENTIK=$(pwgen -s 40 1)" >> .env
     echo "PG_USER=user" >> .env
-    echo "AUTHENTIK_SECRET_KEY=$AUTHENTIK_SECRET_KEY" >> .env
+    echo "AUTHENTIK_SECRET_KEY=$(pwgen -s 50 1)" >> .env
+    echo "MINIO_ACCESS_KEY=$(pwgen -s 16 1)" >> .env
+    echo "MINIO_SECRET_KEY=$(pwgen -s 64 1)" >> .env
 fi
